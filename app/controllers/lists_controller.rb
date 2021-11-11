751918ddad9bf530ad611f14d89d8667cfbc967f
Emily Martinek <emily.martinek12@gmail.com>
2021-11-11 16:52:17 +0100

    jkh

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,23 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list.save!
+
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # references :movie
+  resources :lists, only: [:show, :index, :new]
+  resources :bookmarks, only: [:new]
 end
