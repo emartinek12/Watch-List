5528a47106779778d7fcf44c32f0fed0c8e52002
Emily Martinek <emily.martinek12@gmail.com>
2021-11-12 12:38:25 +0100

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:show, :index, :new]
-  resources :bookmarks, only: [:new]
+  root to: 'lists#index'
+  resources :lists, only: [:show, :index, :new] do
+    resources :bookmarks, only: [:new]
+  end
 end
